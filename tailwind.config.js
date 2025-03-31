2a643c82247e86317113dc558dcef1355418632c
olukayode <olukayodeibroheem@gmail.com>
2025-03-31 13:14:42 +0100

    innitial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+        fontFamily: {
+          young: ['"Young Serif"', "serif"],
+          outfit: ['"Outfit"', "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
